bb35400d419fe8da8ade72ac053135fd090c47eb
Vishal Dhingra <55521133+vishald-au@users.noreply.github.com>
2021-07-02 12:57:38 +0930

    up

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,5 +1,5 @@
 import { GoCheck } from 'react-icons/go'
-import { MdAdd } from 'react-icons/md'
+import { RiArrowGoBackFill } from 'react-icons/ri'
 import { useState } from 'react';
 import Moment from 'react-moment';
 import QuickGraph from './QuickGraph'
@@ -8,6 +8,24 @@ import axios from 'axios';
 
 
 const List = ({ todoData, allValues, getData }) => {
+
+    const [ showPop, setShowPop ] = useState(false)
+
+    const [ popData, setPopData] = useState([])
+
+    const openPop = (item) => {
+        setShowPop(true)
+        setPopData({
+            title: item.title,
+            details: item.details,
+            priority: item.priority,
+            status: item.status,
+            duedate: item.duedate
+        })
+    }
+    const closePop = () => {
+        setShowPop(false)
+    }
 
 
     const filterComplete = () => {
@@ -38,25 +56,19 @@ const List = ({ todoData, allValues, getData }) => {
         )
     }
 
+
  
-
-
 
     return (
         <div className='listingArea'>
             <div className='container-fluid'>
                 <div className='row'>
-                    {/* <div className='col-sm-12 col-md-12 p-3'>
-                        <NavLink to="/add">
-                            <div className='addHelpButton'><MdAdd /></div>
-                        </NavLink>
-                    </div> */}
                     <div className='col-sm-12 col-md-4 maxWidthTable'>
-                        <table class="table table-dark">
+                        <table className='table table-dark'>
                             <thead>
                                 <tr className='modifyHead'>
 
-                                    <th scope="col">Todo</th>
+                                    <th scope='col'>Todo</th>
 
 
                                 </tr>
@@ -64,11 +76,11 @@ const List = ({ todoData, allValues, getData }) => {
                             <tbody className='modifyTable'>
                                 {filterComplete(todoData).map(item => (
 
-                                    <tr onDoubleClick={() => handleStatus(item)} key={item.id} className={item.status === 1 && 'bg-new'} >
+                                    <tr onDoubleClick={() => openPop(item)} key={item.id} className={item.status === 1 && 'bg-new'} >
                                         <td className='modifyTableTD'>{item.priority == 2 ? <span className='priorityBasic pb-high'>H</span> : (item.priority == 1) ? <span className='priorityBasic pb-med'>M</span> : <span className='priorityBasic pb-low'>L</span>}</td>
-                                        <td scope="col-6">#{item.id} {item.title}</td>
+                                        <td scope='col-6'>#{item.id} {item.title}</td>
                                         <td><Moment date={item.duedate} fromNow /></td>
-                                        <td className='text-right'><button className='markButton'><GoCheck /></button></td>
+                                        <td className='text-right'><button onClick={() => handleStatus(item)} className='markButton'><GoCheck /></button></td>
                                     </tr>
 
 
@@ -79,12 +91,12 @@ const List = ({ todoData, allValues, getData }) => {
                         </table>
                     </div>
                     <div className='col-sm-12 col-md-4 maxWidthTable2'>
-                        <table class="table table-dark">
+                        <table className='table table-dark'>
                             <thead>
                                 <tr className='modifyHead'>
 
 
-                                    <th scope="col">Complete</th>
+                                    <th scope='col'>Complete</th>
 
 
                                 </tr>
@@ -92,9 +104,10 @@ const List = ({ todoData, allValues, getData }) => {
                             <tbody className='modifyTable'>
                                 {filterComplete2(todoData).map(item => (
 
-                                    <tr onDoubleClick={() => handleStatus(item)} key={item.id} className={item.status == 1 && 'bg-new'} >
+                                    <tr onDoubleClick={() => openPop(item)} key={item.id} className={item.status == 1 && 'bg-new'} >
 
-                                        <td scope="col-6">#{item.id} {item.title}</td>
+                                        <td scope='col-6'>#{item.id} {item.title} <button  onClick={() => handleStatus(item)} className='assignBack'><RiArrowGoBackFill /></button></td>
+                                        
 
                                     </tr>
 
@@ -106,6 +119,27 @@ const List = ({ todoData, allValues, getData }) => {
                     <div className='col-sm-12 col-md-4'>
                         <QuickGraph allValues={allValues} />
                     </div>
+
+                    <div className={showPop == true ? 'modal bg-blur active' : 'modal bg-blur'} tabindex='-1'>
+                        <div className='modal-dialog modal-dialog-centered'>
+                            <div className='modal-content bg-light text-dark'>
+                            <div className='modal-header'>
+                                <h5 className='modal-title'>{popData.title}</h5>
+                                <button onClick={() => closePop()} type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                            </div>
+                            <div className='modal-body'>
+                                <p>{popData.details}</p>
+                            </div>
+                            <div class="modal-footer">
+                            <span className='badge bg-danger custFont'><Moment date={popData.duedate} fromNow /></span>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
             </div>
         </div>
