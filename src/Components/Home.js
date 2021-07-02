7e9d7fe0dab0952c2df2ed646190907ba6300da1
Vishal Dhingra <55521133+vishald-au@users.noreply.github.com>
2021-07-02 14:35:35 +0930

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,6 +14,9 @@ const Home = () => {
         dispatch(setSignedIn(true))
         dispatch(setUserData(res.profileObj))
     }
+    const responseFail = (err) => {
+        console.log(err);
+    }
 
 
     const googleId = '425457352579-p6rkqogl5ldaha3837cm89ggri0t9t16.apps.googleusercontent.com'
@@ -21,19 +24,26 @@ const Home = () => {
 
     return (
         <>
-            <Dashboard />
-            {/*             {!isSignedIn ?
-                <div className='home'><GoogleLogin
-                    clientId={googleId}
-                    buttonText="Login with Google"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                    isSignedIn={false}
-                /> </div> : <>
+            {/* <Dashboard /> */}
+                        {!isSignedIn ?
+                <div className='home'>
+                
+                        <img className='logoStyle' src='../img/t-logo.png' />
+                        <GoogleLogin
+                        clientId={googleId}
+                        buttonText="Login with Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseFail}
+                        cookiePolicy={'single_host_origin'}
+                        isSignedIn={false}
+                        className='googleStyle'
+                        theme='light'
+                    /> 
+                 
+                </div> : <>
                     <Dashboard />
                 </>
-            } */}
+            }
 
         </>
     )
