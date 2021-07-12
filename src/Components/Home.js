0f53fda7f1a276310fc8d216213809c3b0633d67
Vishal Dhingra <55521133+vishald-au@users.noreply.github.com>
2021-07-12 19:46:48 +0930

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,25 +1,7 @@
-import GoogleLogin from 'react-google-login';
-import { useDispatch, useSelector } from 'react-redux'
-import { selectSignedIn, setSignedIn, setUserData } from '../userSlice';
 import Dashboard from './Dashboard'
 
 const Home = () => {
 
-    const dispatch = useDispatch()
-    const isSignedIn = useSelector(selectSignedIn)
-    //   const userData = useSelector(selectUserData)
-
-    const responseGoogle = (res) => {
-        console.log(res);
-        dispatch(setSignedIn(true))
-        dispatch(setUserData(res.profileObj))
-    }
-    const responseFail = (err) => {
-        console.log(err);
-    }
-
-
-    const googleId = '496571883606-82lsm43keuqf673vrjdnrietll9lrl80.apps.googleusercontent.com'
 
 
     return (
