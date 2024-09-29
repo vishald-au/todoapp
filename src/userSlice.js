3ffd1df88f463f489b91b457b167997094d0358b
vishald-au <dhingra.vishal1@gmail.com>
2024-09-29 23:44:17 +0930

    Fix: reverting user login

--- src/userSlice.js
+++ src/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    isSignedIn: true,
+    isSignedIn: false,
     userData: null,
     allData: null,
   },
